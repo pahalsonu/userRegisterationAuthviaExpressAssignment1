89b0f5b687f2b2c9bd4758bcd99d4244832f4456
pahalsonu <pahalsonu@gmail.com>
2020-12-30 15:26:49 +0530

    frontend done wirh hbs

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-
-const PORT = process.env.port || 2020;
+const AppmonTrial = require("./userSchema/UserSchema")
+const PORT = process.env.port || 3000;
 //mongo db conn
 require('./db/mongoConn')
 
-app.use(express.static(path.join(__dirname,"./public")))
+app.set('view engine', 'html')
+app.engine('html', require('hbs').__express);
+
+app.get('/register', function (req, res) {
+    res.render('register')
+  })
 
 app.get('/', (req, res) =>{
     res.send('User Auth assignment 1')
