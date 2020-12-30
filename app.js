8af359fd469fc6bb0cf37a9abd593196a8212e7b
pahalsonu <pahalsonu@gmail.com>
2020-12-30 13:25:15 +0530

    front end bootstrap form template set to static path

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 
 const PORT = process.env.port || 2020;
 //mongo db conn
 require('./db/mongoConn')
 
+app.use(express.static(path.join(__dirname,"./public")))
 
 app.get('/', (req, res) =>{
     res.send('User Auth assignment 1')
