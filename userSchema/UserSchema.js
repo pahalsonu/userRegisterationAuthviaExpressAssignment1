6c9643b2bc2b81099d95a687112b72f590b941fa
pahalsonu <pahalsonu@gmail.com>
2020-12-30 14:45:01 +0530

    user schme defined

---
+++ userSchema/UserSchema.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const AppmonUserSchema = new mongoose.Schema({
+    first_name :{
+        type : String,
+        required : true
+    },
+    last_name :{
+        type : String,
+    },
+    email :{
+        type : String,
+        required : true,
+        unique : true
+
+    },
+    phone :{
+        type : Number,
+        required : true
+    },
+    Password :{
+        type : String,
+        required : true
+    },
+    ConfirmPassword :{
+        type : String,
+        required : true
+    }
+});
+
+//lets create collection
+
+const AppmonTrial = new mongoose.model("AppmonTrial", AppmonUserSchema)
+
+module.exports = AppmonTrial;
